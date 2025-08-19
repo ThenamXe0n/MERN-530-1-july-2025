255c0c3d56549c43adcfa663f3cebb7cbea292b3
ThenamXe0n <thenameet0@gmail.com>
2025-08-19 23:45:30 +0530

    day 1 - data types in javascript

---
+++ javascript/day1.js
@@ -0,0 +1,50 @@
+//data types in js
+// -primitive
+// -non primitive
+//   ==================primitive Data=============
+
+
+//1. string => // char, para, letters
+// 2.Number => all number
+// NAN (not a number)
+// 3 . boolean // true / false /0/1
+// 4 .undefine
+// 5 null
+// 6 bigInt (number) //big intigers /99n
+
+// 7 symbol es 6
+
+// ------------------------------
+// -object
+// --arrays
+
+// ---------data type checking------------
+// typeof(input)
+// console.log("the data type of word 'hellow' is : ",typeof("hellow")) //string
+// console.log(typeof(123)) //number
+// console.log(typeof(true)) //boolean
+// console.log(typeof(undefined)) //undefined
+// console.log(typeof(null)) //null
+// console.log(typeof("a"*5))//nan
+// console.log(typeof(22n))//bigInt
+
+// console.log("the sum of 5+5 is :",5+5)
+
+console.log(typeof "5");
+console.log(typeof 5);
+
+console.log("5" + 5);
+
+//console print
+// creating a  variable
+
+// keyword(var,let,const) NameOfvariable = "value"
+
+// let x=10
+// var y=6
+
+let x;
+let y;
+// var x="hi"
+console.log("value of x is:", x);console.log("value of y is : ", y);
+console.log("sum of x and y", x + y);
