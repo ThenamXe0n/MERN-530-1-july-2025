885e30b4e985c91212ccdb5b12b40540e41154a2
ThenamXe0n <thenameet0@gmail.com>
2025-09-22 19:16:39 +0530

    dom day 1

---
+++ javascript/DOM/dom-day1.js
@@ -0,0 +1,12 @@
+//DOM in js
+// -dom refers to document(file ie html) object(data type) modal(structure/mapping/drawing)
+//  -root (document entry point)
+//  -element node & text nodes (tag,attribute,content(text node)) 
+
+
+// access dom by document keyword
+document.writeln("this is my text ")
+document.writeln("this is my text ")
+
+//selection methods 
+let heading = document.querySelector('h1')
