8ae0401f254511509abcfe6eebee3bfd1f0b1d9c
ThenamXe0n <thenameet0@gmail.com>
2025-09-23 18:54:24 +0530

    day 2 : dom selection methods

---
+++ javascript/DOM/dom-day2.js
@@ -0,0 +1,30 @@
+// selection methods(functions) and dom pre functions
+// - getElementById
+// - getElementsByClassName
+// - getElementsByTagName
+// - querySelector
+// - querySelectorAll
+
+1; // querySelector
+// let heading = document.querySelector("h1");
+// console.log(heading); // <h1 id="heading">this is heading</h1>
+
+// let heading = document.querySelector("h1");
+// function selection() {
+//   console.log(heading);
+// }
+
+// /select h1 tag present in html file (document)
+// /select p tag present in html file whose class is para (document)
+// /select button tag present in html file whose id is myButton (document)
+
+
+//getElementById
+
+let button = document.getElementById("myButton");
+
+//getElementsclassName
+let paragraphs = document.getElementsByClassName("para");
+
+//getelementstagName
+let h1 = document.getElementsByTagName("myButton");
