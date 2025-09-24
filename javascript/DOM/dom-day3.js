bba1ff19e7968ddcee93ddb8851b7426720df905
ThenamXe0n <thenameet0@gmail.com>
2025-09-24 18:54:37 +0530

    dom elements basic key

---
+++ javascript/DOM/dom-day3.js
@@ -0,0 +1,11 @@
+// key or property of dom element
+
+// id, className, name, tagname, type, value, href, src, style, title, alt, etc attribute
+
+// innerHTML, innerText, textContent,classList
+
+
+let selectedElement =document.getElementById("main_container")
+let imageElement =document.getElementById("myImage")
+let inputElement =document.getElementById("myInput")
+let linkElement =document.getElementById("myLink")
