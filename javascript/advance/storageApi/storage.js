444aedb6e7f946cb917ed3ff90cc8ffd5cf99557
ThenamXe0n <thenameet0@gmail.com>
2025-11-07 19:04:30 +0530

    storage API

---
+++ javascript/advance/storageApi/storage.js
@@ -0,0 +1,9 @@
+//// localstorage
+
+// to store data in localstorage use = > localStorage.setItem("key","value")
+// note : value must be in string format
+
+localStorage.setItem("username", "Mindcoders");
+
+
+//
