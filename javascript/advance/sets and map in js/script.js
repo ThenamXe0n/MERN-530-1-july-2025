a49039f2d8b09e6133c7c12bd7ec3596bffeaf0d
ThenamXe0n <thenameet0@gmail.com>
2025-11-13 18:43:22 +0530

    sets object in js

---
+++ javascript/advance/sets and map in js/script.js
@@ -0,0 +1,37 @@
+//Sets and Maps (data types ) ES6
+
+// - creating a Set object (using new Set())
+let mySet = new Set([
+  "fragrances",
+  "fragrances",
+  "fragrances",
+  "fragrances",
+  "fragrances",
+  "furniture",
+  "furniture",
+  "furniture",
+  "furniture",
+  "furniture",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+  "groceries",
+]);
+console.log(2,"hi",5+5)
+
+//adding items in a set object using add method ==> setName.add("item")
+mySet.add();
+
+
+
+// to remove a value from set object  delete method == >setName.delete()
